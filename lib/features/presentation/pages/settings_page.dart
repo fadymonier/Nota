8b121204e6442fb0a6e7d8298ea093e7ad45b61f
fadymonier <fadymonier111@gmail.com>
2024-12-24 04:40:51 -0800

    finished

--- lib/features/presentation/pages/settings_page.dart
+++ lib/features/presentation/pages/settings_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:easy_localization/easy_localization.dart';
 // import 'package:nota/core/localization/language_provider.dart';
 import 'package:nota/core/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
